0863dea773a6290206e403c25cb4f324c35ad655
Teranveril <sonofselorin@gmail.com>
2025-03-17 00:13:18 +0100

    aaa

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PetController;
+use App\Http\Controllers\PetWebController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/pets', [PetController::class, 'index']);
+Route::get('/pets', [PetWebController::class, 'index']);

dd4a58fbf2803703c4004311d0caf10e9131c6c8
Teranveril <sonofselorin@gmail.com>
2025-03-17 10:09:02 +0100

    aaa

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PetController;
 
-Route::prefix('api')->group(function () {
-    // GET /api/pets - lista wszystkich zwierząt
-    Route::get('/pets', [PetController::class, 'index']);
-
-    // POST /api/pets - dodaj nowe zwierzę
-    Route::post('/pets', [PetController::class, 'store']);
-
-    // GET /api/pets/{id} - szczegóły zwierzęcia
-    Route::get('/pets/{id}', [PetController::class, 'show']);
-
-    // PUT /api/pets/{id} - aktualizuj zwierzę
-    Route::put('/pets/{id}', [PetController::class, 'update']);
-
-    // DELETE /api/pets/{id} - usuń zwierzę
-    Route::delete('/pets/{id}', [PetController::class, 'destroy']);
-});
+Route::get('/pets', [PetController::class, 'index']);
